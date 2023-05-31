3bc6cd1ee7644fed7ed10b1b5cba53e00e123fea
Skjuber <ante.22@hotmail.com>
2023-05-31 19:54:08 +0200

    ContactDetails.tsx created

---
+++ src/components/services/ContactDetails.tsx
@@ -0,0 +1,27 @@
+import React, { useState, useEffect } from "react";
+import { useParams } from "react-router-dom";
+import API from "./api";
+import Contact from "../utils/interfaces/Contact";
+import { RouteParams } from "../utils/interfaces/types";
+
+const ContactDetails: React.FC = () => {
+  const [contact, setContact] = useState<Contact | null>(null);
+  const { id } = useParams<RouteParams>();
+
+  useEffect(() => {
+    API.get(`/Contact/Edit/${id}`).then((response) => {
+      setContact(response.data);
+    });
+  }, [id]);
+
+  return contact ? (
+    <div>
+      <h2>
+        {contact.FirstName} {contact.LastName}
+      </h2>
+      {/* add more details as required */}
+    </div>
+  ) : null;
+};
+
+export default ContactDetails;

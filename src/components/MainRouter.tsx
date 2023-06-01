de768387ee1fc692def1ddc949639fa86fe526b5
toni simac <ante.22@hotmail.com>
2023-06-01 18:55:58 +0200

    Bookmarked contacts added to MainRouter

--- src/components/MainRouter.tsx
+++ src/components/MainRouter.tsx
@@ -4,6 +4,7 @@ import store from "./store/store";
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import ContactList from "./services/ContactList";
 import ContactDetails from "./services/ContactDetails";
+import BookmarkedContacts from "./services/BookmarkedContacts"; // import the new component
 
 const MainRouter: React.FC = () => {
   return (
@@ -12,6 +13,7 @@ const MainRouter: React.FC = () => {
         <Routes>
           <Route path="/" element={<ContactList />} />
           <Route path="/contact/:id" element={<ContactDetails />} />
+          <Route path="/bookmarked" element={<BookmarkedContacts />} /> 
         </Routes>
       </Router>
     </Provider>

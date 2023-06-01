990854dc3ec9054a4b48c2294d076f1e168559d0
toni simac <ante.22@hotmail.com>
2023-06-01 17:14:15 +0200

    ToggleBookmark function added to action.ts file

--- src/components/store/actions.ts
+++ src/components/store/actions.ts
@@ -4,3 +4,9 @@ export const setContacts = (contacts:  Contact[]) => ({
     type: 'SET_CONTACTS',
     payload: contacts
   });
+
+  export const toggleBookmark = (contactId: string) => ({
+    type: 'TOGGLE_BOOKMARK',
+    payload: contactId,
+  });
+  

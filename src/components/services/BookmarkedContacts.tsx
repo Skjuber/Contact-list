615a8a188f1fe90b889c3815233d448d8a774efe
toni simac <ante.22@hotmail.com>
2023-06-01 17:28:58 +0200

    BookmarkedContacts.tsx file created

---
+++ src/components/services/BookmarkedContacts.tsx
@@ -0,0 +1,29 @@
+import React from "react";
+import { useSelector } from 'react-redux';
+import Contact from "../utils/interfaces/Contact";
+import { RootState } from '../store/rootReducer';
+
+
+const BookmarkedContacts: React.FC = () => {
+  const contacts = useSelector((state: RootState) => state.contacts);
+  const bookmarkedContacts = contacts.filter((contact: Contact) => contact.isBookmarked);
+
+  return (
+    <div>
+      <h2>Bookmarked Contacts</h2>
+      {bookmarkedContacts.length > 0 ? (
+        bookmarkedContacts.map((contact: Contact, index: number) => (
+          <div key={contact.id}>
+            <h3>{index + 1}. {contact.FirstName} {contact.LastName}</h3>
+            <p>Email: {contact.Email}</p>
+            {/* display more details as required */}
+          </div>
+        ))
+      ) : (
+        <p>No bookmarked contacts yet.</p>
+      )}
+    </div>
+  );
+};
+
+export default BookmarkedContacts;

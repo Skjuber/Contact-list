c62fc0b2ac544ad067e9d772e4840b57815318c7
Skjuber <ante.22@hotmail.com>
2023-06-01 20:57:28 +0200

    Bookmarking/Unbookmarking funcionality added to BookmarkedContacts page

--- src/components/services/BookmarkedContacts.tsx
+++ src/components/services/BookmarkedContacts.tsx
@@ -1,14 +1,22 @@
 import React from "react";
 import { useSelector } from "react-redux";
+import { useAppDispatch } from "../store/store"; // add this import
+import { toggleBookmark } from "../store/actions"; // add this import
 import Contact from "../utils/interfaces/Contact";
 import { RootState } from "../store/rootReducer";
 import { Link } from "react-router-dom";
 
 const BookmarkedContacts: React.FC = () => {
+  const dispatch = useAppDispatch(); // add this line
   const contacts = useSelector((state: RootState) => state.contacts);
   const bookmarkedContacts = contacts.filter(
     (contact: Contact) => contact.isBookmarked
   );
+
+  const handleBookmarkToggle = (Id: string) => {
+    // add this function
+    dispatch(toggleBookmark(Id));
+  };
 
   return (
     <div>
@@ -20,7 +28,12 @@ const BookmarkedContacts: React.FC = () => {
               {index + 1}. {contact.FirstName} {contact.LastName}
             </h3>
             <p>Email: {contact.Email}</p>
-            {/* display more details as required */}
+            <input
+              type="checkbox"
+              checked={contact.isBookmarked}
+              onChange={() => handleBookmarkToggle(contact.Id)}
+            />{" "}
+            Remove Bookmark
           </div>
         ))
       ) : (

b83391c5c8f0d05285e71e4ffede94aaeb6db187
Skjuber <ante.22@hotmail.com>
2023-06-01 19:44:58 +0200

    Link/button added to Bookmarked articles section,hence,solving the error of bookmarked articles not being rendered

--- src/components/MainRouter.tsx
+++ src/components/MainRouter.tsx
@@ -1,19 +1,24 @@
 import React from "react";
-import { Provider } from 'react-redux';
+import { Provider } from "react-redux";
 import store from "./store/store";
-import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import { BrowserRouter as Router, Route, Routes, Link } from "react-router-dom";
 import ContactList from "./services/ContactList";
 import ContactDetails from "./services/ContactDetails";
-import BookmarkedContacts from "./services/BookmarkedContacts"; // import the new component
+import BookmarkedContacts from "./services/BookmarkedContacts";
 
 const MainRouter: React.FC = () => {
   return (
     <Provider store={store}>
       <Router>
+        <div>
+          <Link to="/bookmarked">
+            <button>View Bookmarked Contacts</button>
+          </Link>
+        </div>
         <Routes>
           <Route path="/" element={<ContactList />} />
           <Route path="/contact/:id" element={<ContactDetails />} />
-          <Route path="/bookmarked" element={<BookmarkedContacts />} /> 
+          <Route path="/bookmarked" element={<BookmarkedContacts />} />
         </Routes>
       </Router>
     </Provider>

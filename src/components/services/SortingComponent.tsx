c12c71cbc313bc8e167c535ef5185594db67d35f
Skjuber <ante.22@hotmail.com>
2023-06-02 18:43:36 +0200

    Styling in progress

--- src/components/services/SortingComponent.tsx
+++ src/components/services/SortingComponent.tsx
@@ -10,11 +10,13 @@ const SortingComponent: React.FC<Props> = ({ onSortChange }) => {
   };
 
   return (
-    <select onChange={handleSortChange}>
-      <option value="firstName">Sort by First Name</option>
-      <option value="lastName">Sort by Last Name</option>
-      <option value="birthDate">Sort by Birth Date</option>
-    </select>
+    <div className="sorting-component">
+      <select className="sort-select" onChange={handleSortChange}>
+        <option value="firstName">Sort by First Name</option>
+        <option value="lastName">Sort by Last Name</option>
+        <option value="birthDate">Sort by Birth Date</option>
+      </select>
+    </div>
   );
 };
 

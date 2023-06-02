a411c06b125a2ef121fd843b47bbb41da9061164
Skjuber <ante.22@hotmail.com>
2023-06-02 23:07:05 +0200

    Blur and focus logic added

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,28 @@
-import React from "react";
+import React, { useEffect } from "react";
 import MainRouter from "./components/MainRouter";
 import "./App.scss";
 
 const App: React.FC = () => {
-  return (
-   
-      <MainRouter />
- 
-   
-  );
+  useEffect(() => {
+    const handleWindowBlur = () => {
+      document.title = "Vrati se :)";
+    };
+
+    const handleWindowFocus = () => {
+      document.title = "inskysolutions";
+    };
+
+    window.addEventListener("blur", handleWindowBlur);
+    window.addEventListener("focus", handleWindowFocus);
+
+    return () => {
+      // Clean up the event listeners when the component is unmounted
+      window.removeEventListener("blur", handleWindowBlur);
+      window.removeEventListener("focus", handleWindowFocus);
+    };
+  }, []); // Empty dependency array means this effect runs once on mount and cleans up on unmount
+
+  return <MainRouter />;
 };
 
 export default App;

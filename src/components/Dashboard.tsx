5d62178e95a5a7a0ba4704a0a8b53bbdfbd12c7e
toni simac <ante.22@hotmail.com>
2023-06-02 19:34:49 +0200

    Dashboard making in progress

--- src/components/Dashboard.tsx
+++ src/components/Dashboard.tsx
@@ -1,5 +1,6 @@
 import React from 'react';
-import { Link } from 'react-router-dom';
+import { Link, useLocation } from 'react-router-dom';
+import { AiFillHome, AiOutlineHome } from 'react-icons/ai';
 import './Dashboard.scss';
 
 interface DashboardProps {
@@ -7,8 +8,15 @@ interface DashboardProps {
 }
 
 const Dashboard: React.FC<DashboardProps> = ({children}) => {
+  const location = useLocation();
+
   return (
     <div className="dashboard">
+      <nav>
+        <Link to="/" className="home-link">
+          {location.pathname === '/' ? <AiFillHome size={24} /> : <AiOutlineHome size={24} />}
+        </Link>
+      </nav>
       <nav>
         <Link to="/bookmarked">
           <button>View Bookmarked Contacts</button>
